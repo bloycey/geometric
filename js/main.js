b6e57ba19550998c4a52f176d203c87e6ba49def
Andrew <bloyce1990@gmail.com>
2017-03-27 09:31:47 +1000

    Off-screen menu working on desktop (need to fix mobile)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+/* Set the width of the side navigation to 250px */
+
+    
+function openNav() {
+    if ($(window).width() > 480) {    
+    document.getElementById("mySidenav").style.width = "250px";
+
+    } else {
+
+    document.getElementById("mySidenav").style.width = "100%";
+}
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+
+
+
+
+
+
+
 
