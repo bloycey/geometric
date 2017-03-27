bf3b68e6e9e298645eb96b01fa309d40cfa3afb8
Andrew <bloyce1990@gmail.com>
2017-03-28 08:54:06 +1000

    Menu Refinements

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
 /* Set the width of the side navigation to 250px */
 
+if ($(window).width() < 480) {
     
+    console.log("mobile width adjusted");
+}
+
+
 function openNav() {
     if ($(window).width() > 480) {    
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("mySidenav").style.height = "100%";   document.getElementById("mySidenav").style.padding = "15px"; 
+    
 
-    } else {
+    } else if ($(window).width() < 480) {
 
-    document.getElementById("mySidenav").style.width = "100%";
+        console.log("Small Nav Clicked");
+        document.getElementById("mySidenav").style.width = "100%";
+        document.getElementById("mySidenav").style.height = "100vh";
+        document.getElementById("mySidenav").style.padding = "15px";
+        
+        
 }
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    if ($(window).width() > 480) {  
+     document.getElementById("mySidenav").style.width = "0";
+     document.getElementById("mySidenav").style.padding = "0";   
+    }
+    else if ($(window).width() < 480) {
+        document.getElementById("mySidenav").style.height = "0";
+        document.getElementById("mySidenav").style.padding = "0";    
+        
+    }
 }
 
 
