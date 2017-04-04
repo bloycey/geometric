f97fb20a7579134a42a2bf2b27dc91878b973580
Andrew <bloyce1990@gmail.com>
2017-04-05 08:48:53 +1000

    Slider panel finished - footer started

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $( document ).ready(function() {
   auto: true,
   autoControls: false
 });
+    $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });   
 });
 
 
